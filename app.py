6fb23d0a583278115dc6d6e7d5aced7a72f977eb
shane-maskell <18406@wc.school.nz>
2022-03-04 11:09:24 +1300

    v4.0 Database working, Sign Up and login page working.

--- app.py
+++ app.py
@@ -43,5 +43,12 @@ def render_menu_page():
 def render_contact_page():
     return render_template('contact.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def render_login_page():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def render_signup_page():
+    return render_template('signup.html')
 
 app.run(host='0.0.0.0', debug=True)
