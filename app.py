20ea6b600334cdb98a8597f853fafbdebdc81fbc
shane-maskell <18406@wc.school.nz>
2022-03-15 10:11:23 +1300

    login hash (not working yet), once logged in, logged out is shown

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from sqlite3 import Error
+from flask_bcrypt import Bcrypt
 
 DB_NAME = "C:/Users/18406/OneDrive - Wellington College/13DTS/Smile/smile.db"
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key = "AHhguwhduYAQjwnIdhwnUdne"
 
 def create_connection(db_file):
-    ###
-    # Create a connection with the database
-    # paramater: name of the database file
-    # returns: a connection to the file
-    ###
+
+    """
+    Create a connection with the database
+    paramater: name of the database file
+    returns: a connection to the file
+    """
     try:
         connection = sqlite3.connect(db_file)
         return connection
@@ -20,10 +24,19 @@ def create_connection(db_file):
 
     return None
 
-
+def is_logged_in():
+    """
+    A function to return whether a user is logged in or not.
+    """
+    if session.get("email") is None:
+        print("Not logged in")
+        return False
+    else:
+        print("Logged in")
+        return True
 @app.route('/')
 def render_homepage():
-    return render_template('home.html')
+    return render_template('home.html', logged_in=is_logged_in())
 
 
 @app.route('/menu')
@@ -36,12 +49,12 @@ def render_menu_page():
     product_list = cur.fetchall()
     con.close()
 
-    return render_template('menu.html', products=product_list)
+    return render_template('menu.html', products=product_list, logged_in=is_logged_in())
 
 
 @app.route('/contact')
 def render_contact_page():
-    return render_template('contact.html')
+    return render_template('contact.html', logged_in=is_logged_in())
 
 @app.route('/login', methods=['GET', 'POST'])
 def render_login_page():
@@ -49,22 +62,36 @@ def render_login_page():
         print(request.form)
         email = request.form.get('email').lower().strip()
         password = request.form.get('password')
-
+        hashed_password = bcrypt.generate_password_hash(password)
         con = create_connection(DB_NAME)
         query = "SELECT id, fname FROM customer WHERE email=? AND password=?"
         cur = con.cursor()
-        cur.execute(query, (email, password))
+        cur.execute(query, (email, hashed_password))
         user_data = cur.fetchall()
         con.close()
 
-        try:
+        if user_data:
             user_id = user_data[0][0]
             first_name = user_data[0][1]
-        except indexError:
+            db_password = user_data[0][2]
+        else:
             return redirect("login?error=Email+or+password+is+incorrect")
-        print(user_id, first_name)
 
-    return render_template('login.html')
+        if not bcrypt.check_password_hash(db_password, password):
+        #Set up a session for this login.
+        session['email'] = email
+        session['username'] = user_id
+        session['fname'] = first_name
+        session['cart'] = []
+        return redirect('/')
+
+    return render_template('login.html', logged_in=is_logged_in())
+
+@app.route('/logout')
+def render_logout_page():
+    [session.pop(key) for key in list(session.keys())]
+    print(list(session.keys()))
+    return redirect("/?message=See+you+next+time")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def render_signup_page():
@@ -83,12 +110,13 @@ def render_signup_page():
         if len(password) < 8:
             return redirect('/signup?error=Passwords+must+be+at+least+8+characters')
 
+        hashed_password = bcrypt.generate_password_hash(password)
         con = create_connection(DB_NAME)
 
         query = "INSERT INTO customer (name, lname, email, password) VALUES (?, ?, ?, ?)"
 
         cur = con.cursor()
-        cur.execute(query, (name,lname, email, password))
+        cur.execute(query, (name,lname, email, hashed_password))
         con.commit()
         con.close()
         return redirect('/login')
@@ -96,7 +124,7 @@ def render_signup_page():
     error = request.args.get('error')
     if error == None:
         error = ""
-    return render_template('signup.html', error=error)
+    return render_template('signup.html', error=error, logged_in=is_logged_in())
 
 
 app.run(host='0.0.0.0', debug=True)
